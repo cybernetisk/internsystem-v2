8261cdbe46f79778000b306752d6dd33234c58ae
Tepohe <ericatsvebakk@gmail.com>
2024-02-22 16:00:50 +0100

    Added simple MUI theme

---
+++ app/components/themeCYB.js
@@ -0,0 +1,20 @@
+
+import { createTheme } from "@mui/material";
+
+export const cybTheme = createTheme({
+  palette: {
+    mode: "light",
+    primary: {
+      main: "#0E0E0E",
+    },
+    secondary: {
+      main: "#d48212",
+    },
+    warning: {
+      main: "#ffe358",
+    },
+    background: {
+      paper: "#E8E8E8",
+    },
+  },
+});
