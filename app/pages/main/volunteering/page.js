ff2f04e0f42823579ebc7d0811588a7e027fd6b7
Tepohe <ericatsvebakk@gmail.com>
2024-07-11 23:40:40 +0200

    Updated volunteer page

--- app/pages/main/volunteering/page.js
+++ app/pages/main/volunteering/page.js
@@ -2,22 +2,31 @@
 "use client";
 
 import { Component, useEffect, useState } from "react";
-import { Box, Button, Card, CardActionArea, Container, Divider, Grid, Paper, Stack, Typography } from "@mui/material";
+import { Box, Card, CardActionArea, CardContent, Container, Divider, Grid, Typography } from "@mui/material";
 import { sanityClient } from "@/sanity/client";
 import PageBuilder from "@/app/components/sanity/PageBuilder";
 import { useRouter } from "next/navigation";
+import authWrapper from "@/app/middleware/authWrapper";
+import { useSession } from "next-auth/react";
+import { cybTheme } from "@/app/components/themeCYB";
 
 
 
 const BUTTON_CONTENT = [
-  { title: "New volunteer", path: "home" },
-  { title: "Volunteer events", path: "" },
-  { title: "Work groups", path: "groups" },
-  { title: "Work logs", path: "" },
-  { title: "Traditions", path: "" },
-  { title: "Finance", path: "" },
-  { title: "Escape wares", path: "" },
-  { title: "User overview", path: "" },
+  { title: "Volunteer groups", path: "groups" },
+  { title: "Stock overview", path: "finance" },
+  { title: "Logs", path: "logs" },
+  {
+    title: "Become volunteer",
+    path: "https://nettskjema.no/a/378483#/page/1",
+    external: true,
+  },
+  { title: "Traditions", path: "", wip: true },
+];
+
+const BUTTON_VOLUNTEER = [
+  { title: "Escape wares", path: "", wip: true },
+  { title: "User overview", path: "", wip: true },
 ];
 
 async function sanityFetch(setPage) {
@@ -43,45 +52,60 @@ async function sanityFetch(setPage) {
   }
 }
 
-export default function VolunteeringPage(params) {
+function VolunteeringPage(params) {
 
   const [page, setPage] = useState(null)
   
+  const session = useSession()
   const router = useRouter()
+  
+  useEffect(() => {
+    
+  })
   
   useEffect(() => {
     sanityFetch(setPage);
   }, []);
   
   const buttons = BUTTON_CONTENT.map((e) => {
-    return (
-      <CustomGridItem
-        key={e.title}
-        label={e.title}
-        path={e.path}
-        onClick={(path) => router.push(`volunteering/${path}`)}
-      />
-    );
+    return <CustomGridItem key={e.title} item={e} router={router}/>
+  });
+  
+  const buttonsVolunteer = BUTTON_VOLUNTEER.map((e) => {
+    return <CustomGridItem key={e.title} item={e} router={router}/>
   });
   
   return (
     <Box>
       <Container disableGutters sx={{ my: 2 }}>
-        <Typography variant="h4">
-          Volunteering
-        </Typography>
+        <Typography variant="h4">Volunteering</Typography>
       </Container>
-      
+
       <Grid container>
-        
-        <Grid item container xs={4} direction="row" spacing={0} alignContent="start">
+        <Grid item container md={2} xs={12} spacing={0} alignContent="start">
           {buttons}
         </Grid>
 
-        <Grid item xs={8} p={1}>
-          {page}
+        <Grid item container md={2} xs={12} spacing={0} p={0} alignContent="start">
+          {/* {session.status == "authenticated" ? buttonsVolunteer : <></>} */}
         </Grid>
-        
+
+        <Grid item md={8} xs={12} p={1}>
+          <Card
+            elevation={1}
+            sx={{
+              // border: "1px solid red",
+              height: "100vh"
+            }}
+          >
+            <CardContent>
+              <Typography variant="h6">Semester overview</Typography>
+              <Divider/>
+              
+            </CardContent>
+          </Card>
+          {/* {page} */}
+        </Grid>
       </Grid>
     </Box>
   );
@@ -91,20 +115,32 @@ class CustomGridItem extends Component {
   
   render() {
     
-    const { label, path, onClick } = this.props
+    const { item, router } = this.props
+    const { title, path, external, wip } = item  
     
     return (
-      <Grid item width="50%" p={1}>
+      <Grid item width="100%" p={1}>
         <Card>
           <CardActionArea
             sx={{ padding: 3 }}
-            onClick={ () => onClick(path) }
+            onClick={() => external ? router.push(path) : router.push(`volunteering/${path}`)}
+          >
+            <Typography variant="body2">{title}</Typography>
+            <Divider variant="fullWidth" />
+            <Typography
+              variant="caption"
+              color={cybTheme.palette.primary.main}
             >
-            <Typography variant="body2">{label}</Typography>
-            <Divider variant="fullWidth"/>
+              {wip != undefined ? "W.I.P" : ""}
+            </Typography>
+            <Typography variant="caption">
+              {external != undefined ? "external url" : ""}
+            </Typography>
           </CardActionArea>
         </Card>
       </Grid>
-    )
+    );
   }
 }
+
+export default authWrapper(VolunteeringPage)
