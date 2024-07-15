71b75778202abad7f35a583d3e0ae63135c0df5b
Tepohe <ericatsvebakk@gmail.com>
2024-07-15 17:02:13 +0200

    Added empty traditions page

---
+++ app/pages/main/volunteering/traditions/page.js
@@ -0,0 +1,13 @@
+import { Box, Divider, Typography } from "@mui/material";
+
+export default function TraditionsPage() {
+  
+  return (
+    <Box>
+      <Typography variant="h4">Traditions</Typography>
+      <Divider sx={{ mb: 4 }}></Divider>
+
+      <Typography>This page is under construction</Typography>
+    </Box>
+  );
+}
