360e7bd54cbd16ef1b33423d5702dfb0ab3d0337
Tepohe <ericatsvebakk@gmail.com>
2024-07-31 18:37:26 +0200

    Minor redesign of volunteer page

--- app/pages/main/volunteering/page.js
+++ app/pages/main/volunteering/page.js
@@ -1,20 +1,26 @@
 
 "use client";
 
-import { Component, useEffect, useState } from "react";
-import { Box, Card, CardActionArea, CardContent, Container, Divider, Grid, Stack, Typography } from "@mui/material";
-import { sanityClient } from "@/sanity/client";
-import PageBuilder from "@/app/components/sanity/PageBuilder";
-import { useRouter } from "next/navigation";
-import authWrapper from "@/app/middleware/authWrapper";
-import { useSession } from "next-auth/react";
+import {
+  Box,
+  Card,
+  CardContent,
+  Divider,
+  Grid,
+  Stack,
+  Typography,
+} from "@mui/material";
+import { useEffect, useState } from "react";
+import { PageHeader, PageHeaderSkeleton } from "@/app/components/sanity/PageBuilder";
 import { cybTheme } from "@/app/components/themeCYB";
 import prismaRequest from "@/app/middleware/prisma/prismaRequest";
 import Link from "next/link";
 
 const BUTTON_CONTENT_1 = [
-  { title: "Stock overview", path: "finance" },
-  { title: "Logs", path: "logs" },
+  { title: "Economy", path: "volunteering/economy" },
+  { title: "Logs", path: "volunteering/logs" },
+  { title: "Membership", path: "volunteering/membership" },
+  { title: "Website content", path: "unauthorized" },
 ];
 
 const BUTTON_CONTENT_2 = [
@@ -23,22 +29,18 @@ const BUTTON_CONTENT_2 = [
     path: "https://nettskjema.no/a/378483#/page/1",
     external: true,
   },
-  { title: "Volunteer groups", path: "groups" },
-  { title: "Traditions", path: "traditions", wip: true },
+  // { title: "Traditions", path: "volunteering/traditions", wip: true },
+  { title: "Volunteer groups", path: "volunteering/groups" },
 ];
 
 
 function VolunteeringPage(params) {
 
   const [paidMemberships, setPaidMemberships] = useState([]);
-  const [usersOnline, setUsersOnline] = useState([]);
   const [voucherLogs, setVoucherLogs] = useState([]);
   const [workLogs, setWorkLogs] = useState([]);
   const [semester, setSemester] = useState(null);
-  const [page, setPage] = useState(null);
-  
-  const session = useSession();
-  const router = useRouter();
+  // const [page, setPage] = useState(null);
   
   useEffect(() => {
     
@@ -57,9 +59,6 @@ function VolunteeringPage(params) {
       model: "semester",
       method: "find",
       request: {
-        where: {
-          id: 23
-        },
         orderBy: {
           id: "desc"
         }
@@ -102,154 +101,121 @@ function VolunteeringPage(params) {
     
   }, [semester])
   
-  const buttons1 = createButtons(BUTTON_CONTENT_1, router);
-  const buttons2 = createButtons(BUTTON_CONTENT_2, router);
-  
-  const currentSemester = semester != undefined ? semester.semester + " " + semester.year : ""
-  const totalWorkHours = workLogs.length != 0 ? workLogs.reduce((tot, cur) => tot += cur.duration,0) : 0;
-  const totalVouchersUsed = voucherLogs.length != 0 ? voucherLogs.reduce((tot, cur) => tot += cur.amount,0) : 0;
-  
   return (
     <Box>
-      <Typography variant="h4" gutterBottom sx={{ fontWeight: "bold" }}>
-        Volunteering
-      </Typography>
-      <Divider sx={{ mb: 4 }}></Divider>
+      <PageHeader text="Volunteering" variant="h4" />
 
-      <Grid container>
-        <Grid item container md={2.5} xs={12} spacing={0} alignContent="start">
-          <Grid item width="100%" p={1}>
-            <Card sx={{ padding: 3 }}>
-              <Box>
-                <Typography variant="body1">Tools</Typography>
-                <Divider variant="fullWidth" sx={{ marginBottom: 1 }}></Divider>
-                {buttons1}
-              </Box>
-              <Box p={3}></Box>
-
-              <Box>
-                <Typography variant="body1">New?</Typography>
-                <Divider variant="fullWidth" sx={{ marginBottom: 1 }}></Divider>
-                {buttons2}
-              </Box>
-            </Card>
-          </Grid>
+      <Grid container direction="row" spacing={2}>
+        <Grid item md={2.5} xs={12}>
+          {createNavigation(semester, paidMemberships, workLogs, voucherLogs)}
         </Grid>
 
-        <Grid
-          item
-          // container
-          md={4.5}
-          xs={12}
-          // spacing={0}
-          p={1}
-          alignContent="start"
-        >
-          <Card sx={{ height: "100vh" }}>
-            <CardContent>
-              <Typography variant="h6">Overview: {currentSemester}</Typography>
-              <Divider />
-
-              <Stack direction="row">
-                <Stack padding={2} spacing={4} direction="column">
-                  <Box>
-                    <Typography variant="body2">Memberships paid:</Typography>
-                    <Typography variant="body2">
-                      {paidMemberships.length}
-                    </Typography>
-                  </Box>
-                  <Box>
-                    <Typography variant="body2">
-                      Total volunteer hours:
-                    </Typography>
-                    <Typography variant="body2">{totalWorkHours}</Typography>
-                  </Box>
-                  <Box>
-                    <Typography variant="body2">
-                      Total vouchers used:
-                    </Typography>
-                    <Typography variant="body2">
-                      {totalVouchersUsed.toFixed(1)} / {totalWorkHours * 0.5}
-                    </Typography>
-                  </Box>
-                </Stack>
-                <Stack padding={2} spacing={8} direction="column"></Stack>
-              </Stack>
-            </CardContent>
-          </Card>
-        </Grid>
-
-        <Grid item md={5} xs={12} p={1}>
-          <Card sx={{ height: "100vh" }}>
-            <CardContent>
-              <Typography variant="h6">something is coming here</Typography>
-              <Divider sx={{ mb: 2 }} />
-              <Typography>Have a nice day :)</Typography>
-            </CardContent>
-          </Card>
+        <Grid item md xs={12}>
+          
         </Grid>
       </Grid>
+      
     </Box>
   );
 }
 
-function createButtons(content, router) {
+function createNavigation(semester, paidMemberships, workLogs, voucherLogs) {
   
-  return content.map((e) => {
-    return (
-      <Link
-        key={"link_" + e.title}
-        href={e.external ? e.path : "/pages/main/volunteering/" + e.path}
-        onClick={() => {
-          console.log(e.path);
-          // router.push("/pages/main/" + e.path);
-        }}
-      >
-        <Typography
-          key={"link_typography" + e.title}
-          variant="body1"
-          gutterBottom
-          sx={{
-            "&:hover": { color: cybTheme.palette.primary.main },
-          }}
-        >
-          {e.title}
-        </Typography>
-      </Link>
-    );
-  });
+  const buttonGroup1 = createButtons(BUTTON_CONTENT_1);
+  const buttonGroup2 = createButtons(BUTTON_CONTENT_2);
+  
+  const currentSemester = semester != undefined ? semester.semester + " " + semester.year : null
+  const membershipsPaid = paidMemberships.length.toLocaleString();
+  const totalWorkHours = workLogs.length != 0 ? workLogs.reduce((tot, cur) => tot += cur.duration,0) : 0;
+  const TWHString = totalWorkHours.toLocaleString();
+  const totalVouchersUsed = voucherLogs.length != 0 ? voucherLogs.reduce((tot, cur) => tot += cur.amount,0) : 0;
+  
+  return (
+    <Grid
+      container
+      direction="column"
+      spacing={2}
+    >
+      <Grid item md={2} xs>
+        <Card>
+          <CardContent>
+            <PageHeader text="TOOLS" variant="body1" gutter={false} />
+            {buttonGroup1}
+            <Divider sx={{ my: 2 }} />
+            {buttonGroup2}
+          </CardContent>
+        </Card>
+      </Grid>
+
+      <Grid item xs alignContent="start">
+        <Card>
+          <CardContent>
+            {currentSemester ? (
+              <>
+                <PageHeader
+                  text={currentSemester}
+                  variant="body1"
+                  gutter={false}
+                />
+              </>
+            ) : (
+              <PageHeaderSkeleton variant={"body1"} gutter={false} />
+            )}
+
+            <Stack spacing={4} direction="column">
+              <Box>
+                <Typography variant="body1">Memberships paid</Typography>
+                <Typography variant="body1">{membershipsPaid}</Typography>
+              </Box>
+              <Box>
+                <Typography variant="body1">Total volunteer hours</Typography>
+                <Typography variant="body1">{TWHString}</Typography>
+              </Box>
+              <Box>
+                <Typography variant="body1">Total vouchers used</Typography>
+                <Typography variant="body1">
+                  {totalVouchersUsed.toFixed(1)} /{" "}
+                  {(totalWorkHours * 0.5).toLocaleString()}
+                </Typography>
+              </Box>
+            </Stack>
+          </CardContent>
+        </Card>
+      </Grid>
+    </Grid>
+  );
 }
 
-// class CustomGridItem extends Component {
+function createButtons(content) {
   
-//   render() {
-    
-//     const { item, router } = this.props
-//     const { title, path, external, wip } = item  
-    
-//     return (
-//       <Grid item width="100%" p={1}>
-//         <Card>
-//           <CardActionArea
-//             sx={{ padding: 3 }}
-//             onClick={() => external ? router.push(path) : router.push(`volunteering/${path}`)}
-//           >
-//             <Typography variant="body2">{title}</Typography>
-//             <Divider variant="fullWidth" />
-//             <Typography
-//               variant="caption"
-//               color={cybTheme.palette.primary.main}
-//             >
-//               {wip != undefined ? "W.I.P" : ""}
-//             </Typography>
-//             <Typography variant="caption">
-//               {external != undefined ? "external url" : ""}
-//             </Typography>
-//           </CardActionArea>
-//         </Card>
-//       </Grid>
-//     );
-//   }
-// }
+  const gridItems = content.map((e) => {
+    return (
+      <Grid item width="100%" key={`grid_${e.title}`}>
+        <Link
+          key={"link_" + e.title}
+          href={e.external ? e.path : "/pages/main/" + e.path}
+        >
+          <Typography
+            key={"link_typography" + e.title}
+            variant="body1"
+            width="100%"
+            sx={{
+              "&:hover": { color: cybTheme.palette.primary.main },
+            }}
+          >
+            {e.title}
+          </Typography>
+        </Link>
+      </Grid>
+    );
+  });
+  
+  return (
+    <Grid item container direction="column" spacing={1}>
+      {gridItems}
+    </Grid>
+  )
+}
+
 
 export default VolunteeringPage
