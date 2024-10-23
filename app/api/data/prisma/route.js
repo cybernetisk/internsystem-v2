1e8dd8f11f0a7ce09edf331944d91a579b4a69b9
Tepohe <ericatsvebakk@gmail.com>
2024-10-23 18:12:11 +0200

    Refactored prisma API route

--- app/api/data/prisma/route.js
+++ app/api/data/prisma/route.js
@@ -1,67 +1,57 @@
 
-
-
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/prisma/prismaClient";
 
 /**
- * @param {*} req
+ * @param {NextRequest} req
  * @returns {NextResponse}
  */
 export async function POST(req) {
-  if (req.method == "POST") {
-    const args = await req.json();
-    let data;
 
-    if (!isValidRequest(args)) {
-      return NextResponse.json(
-        { error: `Request body ${args} is not correct` },
-        { status: 405 }
-      );
-    }
-    
-    if (args.debug) console.log("PMH Request", args);
-    
-    try {
-      switch (args.method) {
-        case "find":
-          data = await prisma[args.model].findMany(args.request);
-          break;
-        case "create":
-          data = await prisma[args.model].create(args.request);
-          break;
-        case "update":
-          data = await prisma[args.model].update(args.request);
-          break;
-        case "delete": 
-          data = await handleDelete(args)
-          break;
-        default:
-          throw new Error(`Invalid method ${args.method} for request`);
-      }
-      
-      if (args.fields) {
-        return NextResponse.json({
-          data: data,
-          fields: Object.keys(prisma[args.model].fields),
-          extra: prisma[args.model].fields,
-        });
-      }
-      
-      return NextResponse.json({ data: data });
+  const args = await req.json();
 
-    } catch (error) {
-      console.log(`uh oh, Error with model "${args.model}"`, error);
-      
-      return NextResponse.json(
-        { error: `Error with ${args.model}: ${error}` },
-        { status: 400 }
-      );
-    }
-  } else {
+  if (!isValidRequest(args)) {
     return NextResponse.json(
-      { error: `Method '${req.method}' does not match POST` },
+      { error: `Request body ${JSON.stringify(args)} is not correct` },
       { status: 405 }
+    );
+  }
+
+  if (args.debug) console.log("PMH Request", args);
+
+  try {
+    let data;
+    switch (args.method) {
+      case "find":
+        data = await prisma[args.model].findMany(args.request);
+        break;
+      case "create":
+        data = await prisma[args.model].create(args.request);
+        break;
+      case "update":
+        data = await prisma[args.model].update(args.request);
+        break;
+      case "delete":
+        data = await handleDelete(args);
+        break;
+      default:
+        throw new Error(`Invalid method ${args.method} for request`);
+    }
+
+    if (args.fields) {
+      return NextResponse.json({
+        data: data,
+        fields: Object.keys(prisma[args.model].fields),
+        extra: prisma[args.model].fields,
+      });
+    }
+
+    return NextResponse.json({ data });
+  } catch (error) {
+    console.log(`uh oh, Error with model "${args.model}"`, error);
+    return NextResponse.json(
+      { error: `Error with ${args.model}: ${error.message}` },
+      { status: 400 }
     );
   }
 }
@@ -70,21 +60,20 @@ function isValidRequest(args) {
   const check1 = Object.hasOwn(args, "model");
   const check2 = Object.hasOwn(args, "method");
   const check3 = Object.hasOwn(args, "request");
-  return check1 & check2 & check3;
+  return check1 && check2 && check3;
 }
 
 async function handleDelete(args) {
-  
   switch (args.model) {
-    case ("user"):
+    case "user":
       const user = await prisma.user.findUnique({
         ...args.request,
         include: {
           activateToken: true,
-          sessions: true
-        }
+          sessions: true,
+        },
       });
-      
+
       const data = await prisma.$transaction([
         prisma.activateToken.deleteMany({
           where: {
@@ -106,12 +95,10 @@ async function handleDelete(args) {
           },
         }),
       ]);
-      
+
       return data;
-      
+
     default:
-      break;
+      throw new Error(`Unsupported model ${args.model} for deletion.`);
   }
-  
-  
 }
