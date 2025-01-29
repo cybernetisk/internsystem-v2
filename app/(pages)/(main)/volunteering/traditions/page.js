f5e2ed246beffe75b13d4ebb1dfe2491f679e0d7
Iver <iver.juul7959@gmail.com>
2025-01-29 21:10:26 +0100

    p2p & sanghefte

--- app/(pages)/(main)/volunteering/traditions/page.js
+++ app/(pages)/(main)/volunteering/traditions/page.js
@@ -7,7 +7,10 @@ export default function TraditionsPage() {
       <Typography variant="h4">Traditions</Typography>
       <Divider sx={{ mb: 4 }}></Divider>
 
-      <Typography>This page is under construction</Typography>
+      {/* <Typography>links</Typography> */}
+      <a href="traditions/song">sanghefte</a>
+      <p></p>
+      <a href="https://docs.google.com/spreadsheets/d/1U6t-q4Zi3yXnckOgN0bAnT4fbL4DnyWJIeIK1cQUOV0/edit?gid=1389238706#gid=1389238706">p2p</a>
     </Box>
   );
 }
