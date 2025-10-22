5d01d78e569027d7721f62b39b88507ffd4ff6dc
Niklas Dietzel <niklas@dietzel.no>
2025-10-22 20:11:22 +0200

    move `NewProduct` and `createProduct` to product.tsx

--- app/(pages)/(main)/volunteering/menu/category.tsx
+++ app/(pages)/(main)/volunteering/menu/category.tsx
@@ -1,13 +1,9 @@
 import { useEffect, useState } from "react";
 import { Button, Grid, Input, Stack, Typography } from "@mui/material";
-import {
-    MenuCategoryCreate,
-    MenuCategoryWithProducts,
-    MenuProductCreate
-} from "@/app/api/v2/escape/menu/products/route";
+import { MenuCategoryCreate, MenuCategoryWithProducts } from "@/app/api/v2/escape/menu/products/route";
 import { MenuCategory } from "@prisma/client";
 
-import { Product } from "@/app/(pages)/(main)/volunteering/menu/product";
+import { NewProduct, Product } from "@/app/(pages)/(main)/volunteering/menu/product";
 
 function updateCategory(category: MenuCategoryWithProducts, newAttributes: Partial<MenuCategory>): Promise<Response> {
 
@@ -138,70 +134,4 @@ export function NewCategory(props: { onUpdate: () => void }) {
 
         </Stack>
     )
-}
-
-function NewProduct(props: { onUpdate: () => void, categoryId: number | null }) {
-    let [productName, setProductName] = useState<string>("");
-    let [productPrice, setProductPrice] = useState<number | null>(null);
-    let [productVolume, setProductVolume] = useState<number | null>(null);
-
-    return (
-        <>
-            <Grid item xs={ 2 }>
-                <Input
-                    type="text"
-                    value={ productName }
-                    onChange={ e => setProductName(e.target.value) }
-                    placeholder="Name"
-                ></Input>
-            </Grid>
-
-            <Grid item xs={ 2 }>
-                <Input
-                    type="number"
-                    value={ productPrice ?? "" }
-                    onChange={ e => setProductPrice(Number(e.target.value)) }
-                    placeholder="Price"
-                ></Input>
-            </Grid>
-
-            <Grid item xs={ 2 }>
-                <Input
-                    type="number"
-                    value={ productVolume ?? "" }
-                    onChange={ e => setProductVolume(Number(e.target.value)) }
-                    placeholder="Volume"
-                ></Input>
-            </Grid>
-
-            <Grid item xs={ 1 }>
-                <Button
-                    onClick={ () => createProduct({
-                        name: productName,
-                        price: productPrice,
-                        volume: productVolume,
-                        priceVolunteer: 0,
-                        glutenfree: 0,
-                        category_id: props.categoryId
-                    }).then(() => {
-                        setProductVolume(null);
-                        setProductPrice(null)
-                        setProductName("");
-                        props.onUpdate();
-                    }) }
-
-                >Create</Button>
-            </Grid>
-        </>
-    );
-}
-
-function createProduct(product: MenuProductCreate): Promise<Response> {
-    return fetch("/api/v2/escape/menu/products", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(product)
-    });
 }
--- app/(pages)/(main)/volunteering/menu/product.tsx
+++ app/(pages)/(main)/volunteering/menu/product.tsx
@@ -1,6 +1,7 @@
 import { MenuProduct } from "@prisma/client";
 import { useEffect, useState } from "react";
-import { Box, Button, Grid, Input, Stack } from "@mui/material";
+import { Button, Grid, Input } from "@mui/material";
+import { MenuProductCreate } from "@/app/api/v2/escape/menu/products/route";
 
 function updateProduct(product: MenuProduct, newAttributes: Partial<MenuProduct>): Promise<Response> {
     return fetch("/api/v2/escape/menu/products", {
@@ -75,4 +76,70 @@ export function Product(props: { product: MenuProduct, onUpdate: () => void }) {
             </Grid>
         </>
     )
+}
+
+export function NewProduct(props: { onUpdate: () => void, categoryId: number | null }) {
+    let [productName, setProductName] = useState<string>("");
+    let [productPrice, setProductPrice] = useState<number | null>(null);
+    let [productVolume, setProductVolume] = useState<number | null>(null);
+
+    return (
+        <>
+            <Grid item xs={ 2 }>
+                <Input
+                    type="text"
+                    value={ productName }
+                    onChange={ e => setProductName(e.target.value) }
+                    placeholder="Name"
+                ></Input>
+            </Grid>
+
+            <Grid item xs={ 2 }>
+                <Input
+                    type="number"
+                    value={ productPrice ?? "" }
+                    onChange={ e => setProductPrice(Number(e.target.value)) }
+                    placeholder="Price"
+                ></Input>
+            </Grid>
+
+            <Grid item xs={ 2 }>
+                <Input
+                    type="number"
+                    value={ productVolume ?? "" }
+                    onChange={ e => setProductVolume(Number(e.target.value)) }
+                    placeholder="Volume"
+                ></Input>
+            </Grid>
+
+            <Grid item xs={ 1 }>
+                <Button
+                    onClick={ () => createProduct({
+                        name: productName,
+                        price: productPrice,
+                        volume: productVolume,
+                        priceVolunteer: 0,
+                        glutenfree: 0,
+                        category_id: props.categoryId
+                    }).then(() => {
+                        setProductVolume(null);
+                        setProductPrice(null)
+                        setProductName("");
+                        props.onUpdate();
+                    }) }
+
+                >Create</Button>
+            </Grid>
+        </>
+    );
+}
+
+function createProduct(product: MenuProductCreate): Promise<Response> {
+    return fetch("/api/v2/escape/menu/products", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(product)
+    });
 }
