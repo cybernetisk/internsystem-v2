14848188f9f8ed1a079be7d10ab00be8b5af75c7
Niklas Dietzel <niklas@dietzel.no>
2026-04-14 18:56:37 +0200

    fix nested <html> tags

--- app/layout.js
+++ app/layout.js
@@ -7,10 +7,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   
   return (
-    <html lang="en">
-      <body>
-        {children}
-      </body>
-    </html>
-  ) 
+        <>{children}</>
+  )
 }
