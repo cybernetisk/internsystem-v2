c1b5464fab7f0215ce28e934eb021b722503e201
Niklas Dietzel <niklas@dietzel.no>
2026-05-03 14:49:02 +0200

    actually fix `authWrapper`
    I misunderstood the `session` object

--- app/middleware/authWrapper.js
+++ app/middleware/authWrapper.js
@@ -5,17 +5,25 @@ import {authClient} from "@/app/api/utils/auth-client.ts";
 export default function authWrapper(WrappedComponent, requiredRole = "", redirect = "/unauthorized") {
     return function AuthenticatedComponent(props) {
 
+
         const router = useRouter();
         const session = authClient.useSession();
 
         if (session.isPending) {
             return <PageBuilderSkeleton/>
-        } else {
+        }
+        else if (!session.data) {
+                router.push(redirect);
+                return <></>;
+            }
+
+         else {
             const userRoles = session.data.user.roles;
             const missingRole = !userRoles.includes(requiredRole)
 
             if (missingRole && !(requiredRole === "")) {
                 router.push(redirect)
+                return <></>;
             }
         }
 
